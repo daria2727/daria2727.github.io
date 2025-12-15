7155976d7d0d931ab30d9c3cdf5e7794b7fbb28f
Daria Vasylieva <daria.vasylieva@ivkhk.ee>
2025-12-15 18:39:50 +0200

    portfolio

--- Arkanoid/game.js
+++ Arkanoid/game.js
@@ -72,7 +72,7 @@ let game = {
     },
     preloadAudio(onResourceLoad) {
         for (let key in this.sounds) {
-            this.sounds[key] = new Audio("sounds/" + key + ".mp3");
+            this.sounds[key] = new Audio("Sounds/" + key + ".mp3");
             this.sounds[key].addEventListener("canplaythrough", onResourceLoad, {once : true});
         }
     },
